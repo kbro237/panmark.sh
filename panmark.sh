ecf84afbcc82b7abb242fe7228cf83dcef9c55d2
Keith Walbolt <keith.walbolt@gmail.com>
2016-12-26 23:06:09 -0500

    now changes working directory

--- panmark.sh
+++ panmark.sh
@@ -3,5 +3,8 @@
 # Marked.app executes scripts in a protected environment, so you have to adjust your path to get the version of interpreters you want panzer to use, e.g. when it executes python filters
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
+# You'll also want to change to your working directory so your scripts can access other files, e.g. images
+cd "$MARKED_ORIGIN"
+
 # Marked.app provides its own environmental variables
 panzer -r markdown -w html -s -S "$MARKED_PATH"
